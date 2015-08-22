2ec5a5c120151abf580a0e6ded89a9953251eb9c
Michael de Hoog <michael.dehoog@gmail.com>
2015-08-22 11:15:18 +1000

    Pushed build

--- packages/ember-runtime/tests/mixins/freezable_test.js
+++
@@ -1,9 +0,0 @@
-import EmberObject from 'ember-runtime/system/object';
-import {Freezable} from 'ember-runtime/mixins/freezable';
-
-QUnit.module('Ember.Freezable');
-
-QUnit.test('should be deprecated', function() {
-  expectDeprecation('`Ember.Freezable` is deprecated, use `Object.freeze` instead.');
-  EmberObject.extend(Freezable).create();
-});
